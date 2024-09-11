8df9e6174038a106a42ea71393542f21f871427a
praveenkumar-gpk <praveengv96@gmail.com>
2024-09-11 10:48:38 +0530

    Event management app

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { configDefaults } from 'vitest/config';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom', // Set the environment to simulate browser-like environment
-    globals: true,        // Use global variables like `describe`, `it`, `expect` without imports
-    setupFiles: './src/setupTests.ts', // Path to setup file
-    exclude: [...configDefaults.exclude, '**/node_modules/**'],
-  },
-});
+})
